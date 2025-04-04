22f2bad0143ecbdea7c15eb87ce0aae320b0c028
Martin Fedorcak <martin.fedorcak44@gmail.com>
2025-04-04 21:12:46 +0200

    Aktualizovany kod ve file

--- HW/HW_Lekce7_Martin Fedorčák.py
+++ HW/HW_Lekce7_Martin Fedorčák.py
@@ -75,54 +75,51 @@ Nakonec vytvořené uživatele vytiskni pomocí cyklu a metody print_user_info.
 # ______________________________________________________________________ #
 
 
-# def create_user():
-#     def is_adult():
-#         while True:
-#             age = int(input("Type your age:"))
-#             if age >= 18:
-#                 user["age"] = age
-#                 break
-#             age_false["success"] = False
-#             age_false["error"] = "Only adults can create an account."
-#             print(age_false)
-#             user_info.append(age_false)
-#
-#
-#
-#
-#     def is_name_valid():
-#         while True:
-#             username = input("Type username for your account:")
-#             if len(username) >= 4:
-#                 user["success"] = True
-#                 user["username"] = username
-#                 break
-#             name_false["success"] = False
-#             name_false["error"] = "Username is too short (4 characters minimum)."
-#             print(name_false)
-#             user_info.append(name_false)
+def create_user():
+    def is_adult():
+        is_running = True
+        while is_running:
+            age = int(input("Type your age:"))
+            if age >= 18:
+                user["age"] = age
+                break
+            age_false["success"] = False
+            age_false["error"] = "Only adults can create an account."
+            is_running = False
+            print(age_false)
+            user_info.append(age_false)
+
+
+    def is_name_valid():
+        while True:
+            username = input("Type username for your account:")
+            if len(username) >= 4:
+                user["success"] = True
+                user["username"] = username
+                break
+            name_false["success"] = False
+            name_false["error"] = "Username is too short (4 characters minimum)."
+            print(name_false)
+            user_info.append(name_false)
+
+
+# Funkce ma chybu, ze po zadani jednoho chybneho udaje do "age", mi funkce preskoci na zadani emailu. Misto toho bz mela skocit zpet na zacatek na zadani username, ale nevim, jak to udelat
+
+# Funkce print_user_info() neni dodelana, protoze jsem nevedel, jak muzu zacyklit funkci create_user() do toho vseho ostatniho
 
 # __________________________________________________________________ #
 
-def create_user():
-    def is_adult():
-        while True:
-            age = int(input("Type your age:"))
-            def is_name_valid():
-                username = input("Type username for your account:")
-                if len(username) >= 4:
-                    user["success"] = True
-                    user["username"] = username
-                    break
-                name_false["success"] = False
-                name_false["error"] = "Username is too short (4 characters minimum)."
-                print(name_false)
-                user_info.append(name_false)
+# def create_user():
+#     def is_name_valid():
+#         username = input("Type username for your account:")
+#         if len(username) < 4:
+#             name_false["success"] = False
+#             name_false["error"] = "Username is too short (4 characters minimum)."
 
 
 
 
-    user_list = []
+    user_list = []      # melo slouzit k ulozeni vsech uzivatelu, odkud bych je pak pridal do funkce print_user_info()
     user = {}
     name_false = {}
     age_false = {}
